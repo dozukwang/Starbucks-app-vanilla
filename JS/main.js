ac8f053c14fb129fa246eeb3f1b511740c052fa6
dozukwang <dozukwang@gmail.com>
2021-10-12 14:11:58 +0900

    feats:devide common.css/js module from mian.css/js
    main.css와 main.js에서 페이지 공통으로 사용하는 내용 분리
    * header
    * footer

--- JS/main.js
+++ JS/main.js
@@ -1,26 +1,3 @@
-// Input 검색바와 Icon 클릭 연동하기
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input')
-
-searchEl.addEventListener('click', function (){
-  searchInputEl.focus();
-  /** input이 소속된 Search 요소 아무거나에 클릭이 감지되면 input 요소에 focus()를 강제 적용 */
-});
-
-// Input 검색바에 focus가 이루어졌을 때의 변화 설정
-searchInputEl.addEventListener('focus', function () {
-  searchEl.classList.add('focused');
-  searchInputEl.setAttribute('placeholder', '통합검색');
-  /** Attribute = html의 속성 */
-  /** Input의 placeholder이란 속성에 '통합검색'이라는 콘텐츠를 추가 */
-});
-
-// Input 검색바에 blur(focus 해제)됐을 때의 변화 설정
-searchInputEl.addEventListener('blur', function () {
-  searchEl.classList.remove('focused');
-  searchInputEl.setAttribute('placeholder', '');
-});
-
 // 특정 높이까지 스크롤되면 badges를 감추기 + 최상단으로 Scroll하기
 const badgeEl = document.querySelector('header .badges');
 const toTopEl = document.querySelector('#to-top');
@@ -153,7 +130,3 @@ spyEls.forEach(function(spyEl) {
     .setClassToggle(spyEl, 'show')
     .addTo(new ScrollMagic.Controller())
 })
-
-// 올해 년도 연동하기
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear(); // 2021
